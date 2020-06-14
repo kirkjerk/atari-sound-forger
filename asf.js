a87dfd735ee470303c47073bb27e81279360472f
kirkjerk <kirkjerk@gmail.com>
2020-06-14 18:40:25 -0400

    piano and some updated map sets

--- asf.js
+++ asf.js
@@ -1,8 +1,8 @@
 /*
 
     TODO:
-        -keyboard
-        -COLOR KEYS wrt AUDC used... may need to not allow punctuation ? 
+        -need a stop all sounds button!
+        -COLOR KEYS wrt AUDC used... 
         -piano keybord
         -record
         -clicktrack
@@ -45,12 +45,13 @@ ASF.maps = defaultMaps; // maps describe how the keyboard maps to various atari 
 
 ASF.clipCache = {}; // map of toneFreqInt2Key to a playable sound file
 ASF.key2kbdDomId = {}; //key code to id of typewriter keyboard key, to light up
+ASF.key2pianoDomId = {}; //key code to id of typewriter keyboard key, to light up
 
 function loadStuff() {
-    document.addEventListener('keydown', go);
-    document.addEventListener('keyup', stop);
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
 
-    ASF.currentMapIndex = 4;
+    ASF.currentMapIndex = 0;
     populateDropdownFromAvailableMaps(ASF.currentMapIndex);
     setupSelectedMap();
 }
@@ -90,26 +91,50 @@ function keyToClipInCurrentMap(key) {
     return clip;
 }
 
-function go(e) {
-    const clip = keyToClipInCurrentMap(e.key);
+function stopAllSounds() {
+    Object.keys(ASF.currentMap.key2tfm).forEach((key) => {
+        stopSound(key);
+    });
+}
+
+function handleKeyDown(e) {
+    startSound(e.key);
+}
+function startSound(keypressed) {
+    const clip = keyToClipInCurrentMap(keypressed);
     if (clip) {
         clip.play();
-        const kbdDom = document.getElementById(ASF.key2kbdDomId[e.key]);
+        const kbdDom = document.getElementById(ASF.key2kbdDomId[keypressed]);
 
         if (kbdDom) {
             kbdDom.classList.add('keydown');
         }
-    } else console.log(`no play for ${e.key}`);
+
+        const pianoDom = document.getElementById(ASF.key2pianoDomId[keypressed]);
+
+        if (pianoDom) {
+            pianoDom.classList.add('keydown');
+        }
+    } else console.log(`no play for ${keypressed}`);
 }
-function stop(e) {
-    const clip = keyToClipInCurrentMap(e.key);
+
+function handleKeyUp(e) {
+    stopSound(e.key);
+}
+function stopSound(keypressed) {
+    const clip = keyToClipInCurrentMap(keypressed);
     if (clip) {
         clip.stop();
-        const kbdDom = document.getElementById(ASF.key2kbdDomId[e.key]);
+        const kbdDom = document.getElementById(ASF.key2kbdDomId[keypressed]);
         if (kbdDom) {
             kbdDom.classList.remove('keydown');
         }
-    } else console.log(`no stop for ${e.key}`);
+        const pianoDom = document.getElementById(ASF.key2pianoDomId[keypressed]);
+
+        if (pianoDom) {
+            pianoDom.classList.remove('keydown');
+        }
+    } else console.log(`no stop for ${keypressed}`);
 }
 
 function ready(fn) {
@@ -121,13 +146,16 @@ function ready(fn) {
 }
 
 function setupSelectedMap() {
+    ASF.key2kbdDomId = {};
     const selectElem = window.mapSelect;
     ASF.currentMap = ASF.maps[selectElem.options[selectElem.selectedIndex].value];
     //ASF.log(`selected tone ${JSON.stringify(ASF.currentMap)}`);
     loadAllFilesForMap(ASF.currentMap);
 
     window.keyboardWrapper.innerHTML = renderUSTypingKeyboard(ASF.currentMap);
-    //  window.pianoWrapper.innerHTML = renderPiano(ASF.currentMap);
+    window.pianoWrapper.innerHTML = renderPiano(ASF.currentMap);
+
+    selectElem.blur();
 }
 
 function populateDropdownFromAvailableMaps(select) {
@@ -142,8 +170,62 @@ function populateDropdownFromAvailableMaps(select) {
 }
 
 function renderPiano(currentMap) {
+    let buf = '';
+    let minOctave = 10;
+    let maxOctave = -1;
+    let hasPianoNotes = false;
+    const note2hunk = {};
+    const note2type = {};
+
+    //for now I'm calling "hunk" the little tone/freq/pianonote map
+
+    //go over each mapping and see if it has piano note attached, figure min/max octaves
+    Object.keys(currentMap.key2tfm).forEach((key) => {
+        const hunk = currentMap.key2tfm[key];
+        if (hunk.p) {
+            note2hunk[hunk.p] = hunk;
+            note2type[hunk.p] = key;
+            hasPianoNotes = true;
+            const octave = hunk.p.slice(1, 2);
+            if (octave < minOctave) minOctave = octave;
+            if (octave > maxOctave) maxOctave = octave;
+        }
+    });
+
+    if (!hasPianoNotes) return '';
+
+    for (let i = minOctave; i <= maxOctave; i++) {
+        buf += renderOctave(currentMap, i, note2type, note2hunk);
+    }
+    return buf;
+}
+
+function renderOctave(currentMap, octave, note2type, note2hunk) {
     const notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
-    return notes.map((note) => `<div>${note}</div>`).join('');
+    return notes.map((note) => renderPianoKey(note, octave, note2type, note2hunk)).join('');
+}
+
+let pianoKeyCounter = 0;
+
+function renderPianoKey(note, octave, note2type, note2hunk) {
+    const sharp = note.length === 2;
+    const notename = note.length === 1 ? `${note}${octave}` : `${note.charAt(0)}${octave}#`;
+
+    const typeKey = note2type[notename] ? `<div>${note2type[notename]}</div>` : '';
+    const disabled = note2type[notename] ? '' : 'disabled';
+
+    const caption = note2type[notename] ? `${notename}${typeKey}` : '';
+
+    const kbdDomId = `piano_${pianoKeyCounter}`;
+    pianoKeyCounter++;
+
+    // note2type[notename] is key they are typing, record the element so we can light it up
+
+    ASF.key2pianoDomId[note2type[notename]] = kbdDomId;
+
+    return !sharp
+        ? `<div class='wrapper'><div id='${kbdDomId}' class='key ${disabled}'>${caption}</div></div>`
+        : `<div class='wrapper'><div id='${kbdDomId}' class='key sharp ${disabled}'>${caption}</div></div>`;
 }
 
 function renderUSTypingKeyboard(currentMap) {
@@ -175,7 +257,7 @@ function renderUSTypingKeyboard(currentMap) {
 
         ASF.key2kbdDomId[c] = kbdDomId;
 
-        return `<div  id='${kbdDomId}'>${c}${soundDisplay}</div>`;
+        return `<div  id='${kbdDomId}'><b>${c}</b>${soundDisplay}</div>`;
     }
 
     const rowsOfKeys = keys.split('\n');
